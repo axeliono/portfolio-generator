072389eca761ff71636fae8c9a54ec7e8ec9651a
axeliono <69375446+axeliono@users.noreply.github.com>
2020-11-21 14:29:32 -0600

    template HTML complete

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const mockData = {
         "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
       languages: ["HTML", "CSS"],
       link: "https://github.com/lernantino/run-buddy",
-      feature: true,
+      feature: false,
       confirmAddProject: true,
     },
     {
